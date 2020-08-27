504a1f0a493718c083275154d4fe7ae5f31ed94e
wangww <372266373@qq.com>
2020-08-28 07:15:06 +0800

    版本更新链接忘记改了，续上

--- app/src/main/java/mobi/gxsd/gxsd_android/MainActivity.java
+++ app/src/main/java/mobi/gxsd/gxsd_android/MainActivity.java
@@ -1400,8 +1400,8 @@ public class MainActivity extends FragmentActivity implements
         this.WhoCheckVersion = who;
 
         Log.d("LM", "检查apk及zip版本");
-//        String Strurl = "https://www.gxsd.mobi/gxsd-dev/read/task/getAppUpdateByAppType?appType=studentAndroid";
-        String Strurl = "https://www.gxsd.mobi/gxsd-dev/read/task/getAppUpdateByAppType?appType=teacherAndroid";
+        String Strurl = "https://www.gxsd.mobi/gxsd-dev/read/task/getAppUpdateByAppType?appType=studentAndroid";
+//        String Strurl = "https://www.gxsd.mobi/gxsd-dev/read/task/getAppUpdateByAppType?appType=teacherAndroid";
 
         HttpURLConnection conn=null;
         try {
