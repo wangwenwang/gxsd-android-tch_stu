e2784dd973eb0e9552a24fca313e323ef8d7fc65
wangww <372266373@qq.com>
2020-09-18 23:17:23 +0800

    1.0.4 -- 6 老师端（内置vue版本1.2.0，zip为1.1.7）

--- app/src/main/java/mobi/gxsd/gxsd_android/Tools/Constants.java
+++ app/src/main/java/mobi/gxsd/gxsd_android/Tools/Constants.java
@@ -9,11 +9,11 @@ public class Constants {
 //    public static final String WXLogin_AppID="wx2b41f56e818f1e1c";
 //    public static final String WXLogin_AppSecret="65ad80aa8111fee81358590e8783e7d1";
     // 学生版student
-    public static final String WXLogin_AppID="wxdce29e3a76e0c723";
-    public static final String WXLogin_AppSecret="65bdac5dee3677bb8150b22a87abece7";
+//    public static final String WXLogin_AppID="wxdce29e3a76e0c723";
+//    public static final String WXLogin_AppSecret="65bdac5dee3677bb8150b22a87abece7";
     // 老师版teacher
-//    public static final String WXLogin_AppID="wxe6d00f631717f3c1";
-//    public static final String WXLogin_AppSecret="63f13ceaad54fe8ee638dc08052ba5b6";
+    public static final String WXLogin_AppID="wxe6d00f631717f3c1";
+    public static final String WXLogin_AppSecret="63f13ceaad54fe8ee638dc08052ba5b6";
 
     public class URL {
         public static  final String SAAS_API_BASE = "http://mobile.kaidongyuan.com/easyToSell/";
