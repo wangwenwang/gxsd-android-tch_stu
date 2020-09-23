2012b4f40a1cc473c118f4d7a2c3e685c5321963
wangww <372266373@qq.com>
2020-09-23 21:45:54 +0800

    1.0.5 -- 8 老师端（内置vue版本1.2.1，zip为1.2.1）

--- app/src/main/java/mobi/gxsd/gxsd_android/MainActivity.java
+++ app/src/main/java/mobi/gxsd/gxsd_android/MainActivity.java
@@ -149,7 +149,7 @@ public class MainActivity extends FragmentActivity implements
     private Uri mImageUri;
     private static final String FILE_PROVIDER_AUTHORITY = "mobi.gxsd.gxsd_android.fileprovider";
 
-    private String CURR_ZIP_VERSION = "1.0.8";
+    private String CURR_ZIP_VERSION = "1.2.1";
     private String WhoCheckVersion;
 
     //检测版本更新
@@ -188,7 +188,7 @@ public class MainActivity extends FragmentActivity implements
 
                     PackageManager pm = getPackageManager();
                     boolean permission = (PackageManager.PERMISSION_GRANTED ==
-                            pm.checkPermission("android.permission.WRITE_EXTERNAL_STORAGE", "mobi.gxsd.gxsd_android_student"));
+                            pm.checkPermission("android.permission.WRITE_EXTERNAL_STORAGE", "mobi.gxsd.gxsd_android_teacher"));
                     if (permission) {
                         checkVersion("原生");
                         break;
@@ -1508,8 +1508,8 @@ public class MainActivity extends FragmentActivity implements
         this.WhoCheckVersion = who;
 
         Log.d("LM", "检查apk及zip版本");
-        String Strurl = "https://www.gxsd.mobi/gxsd-test/read/task/getAppUpdateByAppType?appType=studentAndroid";
-//        String Strurl = "https://www.gxsd.mobi/gxsd-test/read/task/getAppUpdateByAppType?appType=teacherAndroid";
+//        String Strurl = "https://www.gxsd.mobi/gxsd-test/read/task/getAppUpdateByAppType?appType=studentAndroid";
+        String Strurl = "https://www.gxsd.mobi/gxsd-test/read/task/getAppUpdateByAppType?appType=teacherAndroid";
 
         HttpURLConnection conn=null;
         try {
