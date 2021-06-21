748ecabb87454d95529d8e09f50099f416808d48
wangww <372266373@qq.com>
2021-06-21 19:33:10 +0800

    解决 android 11 添加了沙盒机制导致拍照无效问题

--- app/src/main/java/mobi/gxsd/gxsd_android/MainActivity.java
+++ app/src/main/java/mobi/gxsd/gxsd_android/MainActivity.java
@@ -174,7 +174,6 @@ public class MainActivity extends FragmentActivity implements
     //拍照图片路径
     private String cameraFielPath;
     private Uri mImageUri;
-    private static final String FILE_PROVIDER_AUTHORITY = "mobi.gxsd.gxsd_android.fileprovider";
 
     private String CURR_ZIP_VERSION = "1.9.1";
     private String WhoCheckVersion;
@@ -208,6 +207,8 @@ public class MainActivity extends FragmentActivity implements
     private RotateLoading rotateLoading;
 
     MediaPlayer mMediaPlayer;
+
+    private Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -725,25 +726,32 @@ public class MainActivity extends FragmentActivity implements
         startActivityForResult(Intent.createChooser(i, "Image Chooser"), FILE_CHOOSER_RESULT_CODE);
     }
 
+    //当前资源裁剪后保存的目标位置
+    private Uri getDestinationUri()  {
+        String fileName = String.format("fr_crop_%s.jpg", System.currentTimeMillis());
+        File cropFile = null;
+        try {
+            cropFile = File.createTempFile(fileName, "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Uri.fromFile(cropFile);
+    }
+
     //拍照
     private void takeCamera() {
+
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if (hasSDCard()) {
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            String imageFileName = "JPEG_" + timeStamp + "_";
-            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            File imageFile;
-            try {
-
-                imageFile = File.createTempFile(imageFileName, ".jpg", storageDir);
-                cameraFielPath = imageFile.getPath();
-            } catch (IOException e) {
-
-                e.printStackTrace();
+        if (hasSDCard() && intent.resolveActivity(getPackageManager()) != null) {
+            photoUri = getDestinationUri();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                // 适配Android 7.0文件权限，通过FileProvider创建一个content类型的Uri
+                photoUri = FileProvider.getUriForFile(this, "mobi.gxsd.gxsd_android.fileprovider", new File(photoUri.getPath()));
+            } else {
+                photoUri = getDestinationUri();
             }
-            File outputImage = new File(cameraFielPath);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(outputImage));
+            // android11以后强制分区存储，外部资源无法访问，所以添加一个输出保存位置，然后取值操作
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
             startActivityForResult(intent, FILE_CAMERA_RESULT_CODE);
         }
     }
@@ -758,26 +766,6 @@ public class MainActivity extends FragmentActivity implements
 
         return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
     }
-
-    private void takeCameraM() {
-        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//打开相机的Intent
-        if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {//这句作用是如果没有相机则该应用不会闪退，要是不加这句则当系统没有相机应用的时候该应用会闪退
-            File imageFile = createImageFile();//创建用来保存照片的文件
-            if (imageFile != null) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    /*7.0以上要通过FileProvider将File转化为Uri*/
-                    mImageUri = FileProvider.getUriForFile(this, FILE_PROVIDER_AUTHORITY, imageFile);
-                } else {
-                    /*7.0以下则直接使用Uri的fromFile方法将File转化为Uri*/
-                    mImageUri = Uri.fromFile(imageFile);
-                }
-                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);//将用于输出的文件Uri传递给相机
-                startActivityForResult(takePhotoIntent, FILE_CAMERA_RESULT_CODE);//打开相机
-            }
-        } else {
-        }
-    }
-
 
     /**
      * 创建用来存储图片的文件，以时间来命名就不会产生命名冲突
@@ -853,7 +841,6 @@ public class MainActivity extends FragmentActivity implements
 
 
             Log.d("LM", "拍照5.9.1: ");
-            takeCameraM();
         }
         else if(requestCode== 1089) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -932,17 +919,9 @@ public class MainActivity extends FragmentActivity implements
         Uri result = null;
         if (requestCode == FILE_CAMERA_RESULT_CODE) {
 
-            if (result == null && hasFile(cameraFielPath)) {
-
-                result = Uri.fromFile(new File(cameraFielPath));
-            }
-            if (uploadMessageAboveL != null) {
-
-                uploadMessageAboveL.onReceiveValue(new Uri[]{result});
-
-                uploadMessageAboveL = null;
+            if (photoUri != null) {
+                uploadMessageAboveL.onReceiveValue(new Uri[]{photoUri});
             } else if (uploadMessage != null) {
-
                 uploadMessage.onReceiveValue(result);
                 uploadMessage = null;
             }
